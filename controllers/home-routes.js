3ccc79f43c65a888c71a2414c3926f7d6b86881d
Berserk9000 <cruz.d.kevin@gmail.com>
2022-09-14 19:53:29 -0400

    login logout routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,6 @@
+const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Post, User, Comment } = require('../models');
+const { Post, User, Comment, Vote } = require('../models');
 
 
 router.get('/', (req, res) => {
@@ -39,4 +40,12 @@ router.get('/', (req, res) => {
       });
   });
 
+  router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('login');
+  });
 module.exports = router;
